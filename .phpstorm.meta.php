f5f15a3a044ad28464b7b530aa4fcddc89874c2c
dapomakanjuola <dm262@njit.edu>
2019-05-17 09:17:15 -0400

    created UserTableSeeder

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-05-17 07:31:41.
+    * Generated on 2019-05-17 13:11:40.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
